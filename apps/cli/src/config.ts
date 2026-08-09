ed01a14b68b3a73d59ef17dc4a6e70a65e551927
Git Sync <git-sync@localhost>
2026-08-09 10:38:15 -0700

    chore(release): bump spur to 0.3.37

--- apps/cli/src/config.ts
+++ apps/cli/src/config.ts
@@ -2,7 +2,7 @@
 export const CLI_CONFIG = {
     binaryName: 'spur',
     binaryLabel: 'spur',
-    binaryVersion: '0.3.36',
+    binaryVersion: '0.3.37',
     configDir: '.spur',
     configFile: '.spur/config.yaml',
     databaseFile: '.spur/spur.db',
